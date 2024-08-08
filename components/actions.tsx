d0ff6b3c980b9dffd82f9ab4414895cd62b9d54b
Piyush <pshtiwari0@gmail.com>
2024-08-09 00:14:04 +0530

    Options to copy and delete a board on dropdown

---
+++ components/actions.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import {
+  DropdownMenuContentProps,
+  DropdownMenuSubContentProps,
+} from "@radix-ui/react-dropdown-menu";
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { Link2, Trash2 } from "lucide-react";
+import { toast } from "sonner";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+
+interface ActionsProps {
+  children: React.ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuSubContentProps["sideOffset"];
+  id: string;
+  title: string;
+}
+
+export const Actions = ({
+  children,
+  side,
+  sideOffset,
+  id,
+  title,
+}: ActionsProps) => {
+  const { mutate, pending } = useApiMutation(api.board.remove);
+
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => {
+        toast.success("Link copied!");
+      })
+      .catch((err) => {
+        toast.error("Failed to copy link!");
+      });
+  };
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => {
+        toast.success("Board deleted!");
+      })
+      .catch((err) => {
+        toast.error("Failed to delete board!");
+      });
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(e) => e.stopPropagation()}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+      >
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy board link
+        </DropdownMenuItem>
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onDelete}>
+          <Trash2 className="h-4 w-4 mr-2" />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
